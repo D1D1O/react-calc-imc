c7bf2487b52cafdb52c568bdc903bb6ac2e15ef7
Giovanny L. Silva <didiosilva395@gmail.com>
2022-08-27 22:13:38 -0400

    end

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import { useState } from 'react';
 import styles from './App.module.css';
 import powerImage from './assets/powered.png';
 import Swal from 'sweetalert2';
-import { levels, calculateImc } from './helpers/imc';
+import { levels, calculateImc ,Level} from './helpers/imc';
 import { GridItem } from './components/GridItem';
+import  leftArrowImage  from './assets/leftarrow.png';
 
 const App =() =>{
 
   const [heightFiel, setHeightField] = useState<number >(0);
   const [weightFiel, setWeightField] = useState<number >(0);
+  const [toShow, setToShow] = useState<Level | null >(null);
 
   const handleCalculateButton = () =>{
     if (heightFiel && weightFiel){
-
-
+      setToShow(calculateImc(heightFiel,weightFiel));
     } else {
       Swal.fire({
         icon: 'error',
@@ -21,6 +22,12 @@ const App =() =>{
         text: 'Não foi possícel calular seu IMC, informe todos os campos !'
       })
     }
+  }
+
+  const handleBackButton = ()=>{
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
   }
 
 
@@ -43,6 +50,7 @@ const App =() =>{
             placeholder='Digite a sua altura. Ex: 1.5 (em metros) '
             value={heightFiel > 0 ? heightFiel : ''}
             onChange={(e)=>setHeightField(parseFloat(e.target.value)) }
+            disabled={toShow ? true : false}
 
           />
 
@@ -51,10 +59,11 @@ const App =() =>{
             placeholder='Digite o seu peso. Ex: 79.5 (em Kg) '
             value={weightFiel > 0 ? weightFiel : ''}
             onChange={(e)=>setWeightField(parseFloat(e.target.value)) }
-
+            disabled={toShow ? true : false}
           />
           <button
             onClick={handleCalculateButton}
+            disabled={toShow ? true : false}
           >
             Calcular
           </button>
@@ -63,13 +72,26 @@ const App =() =>{
 
         </div>
         <div className={styles.rightSide}>
-          <div className={styles.grid} >
-            { 
-              levels.map((item,key)=>(
-                <GridItem key={key} item={item}  /> 
-              ) )
-            }
-          </div>
+          
+          {!toShow && 
+            <div className={styles.grid} >
+              { 
+                levels.map((item,key)=>(
+                  <GridItem key={key} item={item}  /> 
+                ) )
+              }
+            </div>
+          }
+          {toShow &&
+            <div className={styles.rightBig} >
+              <div className={styles.rightArrow} onClick={handleBackButton}  >
+                <img src={leftArrowImage} alt=""  width={25}/>
+              </div>
+              <GridItem item={toShow} />
+            </div>
+
+          }
+
         </div>
       </div>
 
